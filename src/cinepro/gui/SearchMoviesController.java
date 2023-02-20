3bb5b460f4d22ca836779f00f5ac4e6ac05bba5f
selimboulaaba <selimboulaaba@outlook.fr>
2023-02-21 00:59:10 +0100

    searhFilm

---
+++ src/cinepro/gui/SearchMoviesController.java
@@ -0,0 +1,21 @@
+package cinepro.gui;
+
+import javafx.application.Application;
+import javafx.scene.input.InputMethodEvent;
+import javafx.stage.Stage;
+
+public class AddProjectionController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+    public void searchMovie(InputMethodEvent inputMethodEvent) {
+        System.out.println("test");
+    }
+}
